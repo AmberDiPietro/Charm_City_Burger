aa984cb3d8638c62844c332e54176db11527defd
AmberDiPietro <103202940+AmberDiPietro@users.noreply.github.com>
2022-11-09 20:49:07 -0500

    fixed menu bar selections

--- Jquery/jquery.js
+++ Jquery/jquery.js
@@ -2,7 +2,7 @@
 var btnContainer = document.getElementById("myDIV");
 
 // Get all buttons with class="btn" inside the container
-var btns = btnContainer.getElementsByClassName("btn");
+var btns = btnContainer.getElementsByClassName("btnz");
 
 // Loop through the buttons and add the active class to the current/clicked button
 for (var i = 0; i < btns.length; i++) {
